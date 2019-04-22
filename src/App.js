3bc54d136636a9d60fe4b8352c86618f61b6629f
Domingo Pérez Guerrero <Domingoperezguerrero@gmail.com>
2019-04-22 16:10:15 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            p4pupro
+            p4pupro & Kenelmita
           </p>
           <a
             className="App-link"
